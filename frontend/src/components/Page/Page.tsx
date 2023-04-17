bd38fcbf77de6989dca2d7745ebc5b4390042979
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-04-17 03:33:13 +0200

    Configured Page layout

--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import Footer from '#components/Footer/Footer';
+import Navbar from '#components/Navbar/Navbar';
 
 import style from './Page.module.scss';
 
@@ -10,7 +12,9 @@ export interface PageProps {
 export default function Page(props: PageProps) {
   return (
     <div className={style.container}>
+      <Navbar />
       {props.children}
+      <Footer />
     </div>
   );
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,9 @@
 import Page from '#components/Page/Page';
-import Footer from '#components/Footer/Footer';
-import Navbar from '#components/Navbar/Navbar';
 
 export default function Home() {
   return (
     <Page>
-      <Navbar />
-      <Footer />
+
     </Page>
   );
 }
