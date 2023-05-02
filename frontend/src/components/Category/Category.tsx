ee8f9022d848a78902f1c5aed6558ff90ce9fa61
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-05-02 17:40:50 +0200

    Integrated categories list with backend

--- frontend/src/components/Category/Category.tsx
+++ frontend/src/components/Category/Category.tsx
@@ -3,13 +3,14 @@ import Button from '#components/Button/Button';
 import { truncateNumber } from '#shared/numberUtils';
 
 import style from './Category.module.scss';
-
+import { useNavigate } from 'react-router-dom';
 
 interface TagProps {
   name: string;
 }
 
 export interface CategoryProps {
+  id: string;
   title: string;
   developer: string;
   tags: string[];
@@ -23,9 +24,7 @@ export interface CategoryProps {
 }
 
 function Tag(props: TagProps) {
-  return (
-    <span className={style.tag}>{props.name}</span>
-  )
+  return <span className={style.tag}>{props.name}</span>;
 }
 
 function Rating(props: { rating: number }) {
@@ -35,8 +34,7 @@ function Rating(props: { rating: number }) {
   if (props.rating < 30) {
     colorClass = style.sad;
     symbol = 'sentiment_very_dissatisfied';
-  }
-  else if (props.rating > 70) {
+  } else if (props.rating > 70) {
     colorClass = style.happy;
     symbol = 'sentiment_very_satisfied';
   }
@@ -46,10 +44,12 @@ function Rating(props: { rating: number }) {
       <MaterialSymbol symbol={symbol} class={style.icon} />
       {props.rating}%
     </span>
-  )
+  );
 }
 
 export default function Category(props: CategoryProps) {
+  const navigate = useNavigate();
+
   return (
     <div className={style.container}>
       <div className={style.backgroundImageContainer}>
@@ -62,13 +62,15 @@ export default function Category(props: CategoryProps) {
         <span className={style.title}>{props.title}</span>
 
         <div className={style.tags}>
-          {props.tags.map((tag) => <Tag key={tag} name={tag} />)}
+          {props.tags.map((tag) => (
+            <Tag key={tag} name={tag} />
+          ))}
         </div>
 
         <div className={style.info}>
           <Rating rating={props.rating} />
           <span className={style.entry}>
-            <MaterialSymbol symbol='group' class={style.icon} />
+            <MaterialSymbol symbol="group" class={style.icon} />
             {truncateNumber(props.players)}
           </span>
         </div>
@@ -78,17 +80,24 @@ export default function Category(props: CategoryProps) {
         <span className={style.achievementTitle}>Achievements</span>
 
         <div className={style.achievementContent}>
-          <MaterialSymbol symbol='military_tech' class={style.icon} />
+          <MaterialSymbol symbol="military_tech" class={style.icon} />
           {props.achievements} of {props.totalAchievements}
         </div>
       </div>
 
-      <Button className={style.button}>PLAY</Button>
+      <Button
+        className={style.button}
+        onClick={() => {
+          navigate(`/quiz/${props.id}`);
+        }}
+      >
+        PLAY
+      </Button>
 
       <div className={style.author}>
         created by
         <span className={style.name}>{props.author}</span>
       </div>
     </div>
-  )
+  );
 }
--- frontend/src/pages/Categories/Categories.tsx
+++ frontend/src/pages/Categories/Categories.tsx
@@ -1,105 +1,123 @@
 import Page from '#components/Page/Page';
 import Category, { CategoryProps } from '#components/Category/Category';
 import Carousel from 'react-multi-carousel';
-import "react-multi-carousel/lib/styles.css";
+import 'react-multi-carousel/lib/styles.css';
+import { useEffect, useState } from 'react';
+import { GetQuizListResponse } from '#types/api/quiz';
 
 import style from './Categories.module.scss';
-import csgo from '#assets/grids/csgo.webp';
-import valorant from '#assets/grids/valorant.webp';
-import fortnite from '#assets/grids/fortnite.webp';
-import lol from '#assets/grids/lol.webp';
+// import csgo from '#assets/grids/csgo.webp';
+// import valorant from '#assets/grids/valorant.webp';
+// import fortnite from '#assets/grids/fortnite.webp';
+// import lol from '#assets/grids/lol.webp';
 
-
-const categories: CategoryProps[] = [
-  {
-    developer: 'Valve',
-    title: 'Counter-Strike: Global Offensive',
-    tags: ['FPS', 'Shooter', 'Multiplayer'],
-    rating: 72,
-    players: 2302,
-    achievements: 6,
-    totalAchievements: 23,
-    author: 'quiztec team',
-    backgroundImageUrl: csgo,
-  },
-  {
-    developer: 'Riot Games',
-    title: 'Valorant',
-    tags: ['FPS', 'Shooter', 'Multiplayer'],
-    rating: 22,
-    players: 214,
-    achievements: 6,
-    totalAchievements: 23,
-    author: 'quiztec team',
-    backgroundImageUrl: valorant,
-  }, {
-    developer: 'Epics Games',
-    title: 'Fortnite',
-    tags: ['Battle Royale', 'Shooter', 'Multiplayer'],
-    rating: 55,
-    players: 112,
-    achievements: 6,
-    totalAchievements: 23,
-    author: 'quiztec team',
-    backgroundImageUrl: fortnite,
-  },
-  {
-    developer: 'Riot Games',
-    title: 'League of Legends',
-    tags: ['MOBA', 'Strategy', 'Multiplayer'],
-    rating: 50,
-    players: 723,
-    achievements: 6,
-    totalAchievements: 23,
-    author: 'quiztec team',
-    backgroundImageUrl: lol,
-  }
-];
+// const categories: CategoryProps[] = [
+//   {
+//     developer: 'Valve',
+//     title: 'Counter-Strike: Global Offensive',
+//     tags: ['FPS', 'Shooter', 'Multiplayer'],
+//     rating: 72,
+//     players: 2302,
+//     achievements: 6,
+//     totalAchievements: 23,
+//     author: 'quiztec team',
+//     backgroundImageUrl: csgo,
+//   },
+//   {
+//     developer: 'Riot Games',
+//     title: 'Valorant',
+//     tags: ['FPS', 'Shooter', 'Multiplayer'],
+//     rating: 22,
+//     players: 214,
+//     achievements: 6,
+//     totalAchievements: 23,
+//     author: 'quiztec team',
+//     backgroundImageUrl: valorant,
+//   }, {
+//     developer: 'Epics Games',
+//     title: 'Fortnite',
+//     tags: ['Battle Royale', 'Shooter', 'Multiplayer'],
+//     rating: 55,
+//     players: 112,
+//     achievements: 6,
+//     totalAchievements: 23,
+//     author: 'quiztec team',
+//     backgroundImageUrl: fortnite,
+//   },
+//   {
+//     developer: 'Riot Games',
+//     title: 'League of Legends',
+//     tags: ['MOBA', 'Strategy', 'Multiplayer'],
+//     rating: 50,
+//     players: 723,
+//     achievements: 6,
+//     totalAchievements: 23,
+//     author: 'quiztec team',
+//     backgroundImageUrl: lol,
+//   }
+// ];
 
 export default function Categories() {
+  const [categories, setCategories] = useState<CategoryProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('http://localhost:3000/api/v1/quiz/list')
+      .then((resp) => resp.json() as Promise<GetQuizListResponse>)
+      .then((data) => {
+        setCategories(data.data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <Page>
-      <Carousel
-        swipeable={true}
-        draggable={false}
-        keyBoardControl={true}
-        containerClass={style.container}
-        infinite={true}
-        centerMode={true}
-        sliderClass={style.slider}
-        responsive={{
-          alpha: {
-            breakpoint: { max: 3000, min: 1920 },
-            items: 4,
-          },
-          beta: {
-            breakpoint: { max: 1920, min: 1725 },
-            items: 3.5,
-          },
-          gamma: {
-            breakpoint: { max: 1725, min: 1530 },
-            items: 3,
-          },
-          delta: {
-            breakpoint: { max: 1530, min: 1335 },
-            items: 2.5,
-          },
-          epsilon: {
-            breakpoint: { max: 1335, min: 1140 },
-            items: 2,
-          },
-          zeta: {
-            breakpoint: { max: 1140, min: 945 },
-            items: 1.5,
-          },
-          eta: {
-            breakpoint: { max: 945, min: 0 },
-            items: 1,
-          },
-        }}
-      >
-          {categories.map((category, index) => <Category key={index} {...category} />)}
+      {!loading && (
+        <Carousel
+          swipeable={true}
+          draggable={false}
+          keyBoardControl={true}
+          containerClass={style.container}
+          infinite={true}
+          centerMode={true}
+          sliderClass={style.slider}
+          responsive={{
+            alpha: {
+              breakpoint: { max: 3000, min: 1920 },
+              items: 4,
+            },
+            beta: {
+              breakpoint: { max: 1920, min: 1725 },
+              items: 3.5,
+            },
+            gamma: {
+              breakpoint: { max: 1725, min: 1530 },
+              items: 3,
+            },
+            delta: {
+              breakpoint: { max: 1530, min: 1335 },
+              items: 2.5,
+            },
+            epsilon: {
+              breakpoint: { max: 1335, min: 1140 },
+              items: 2,
+            },
+            zeta: {
+              breakpoint: { max: 1140, min: 945 },
+              items: 1.5,
+            },
+            eta: {
+              breakpoint: { max: 945, min: 0 },
+              items: 1,
+            },
+          }}
+        >
+          {categories.map((category, index) => (
+            <Category key={index} {...category} />
+          ))}
         </Carousel>
+      )}
     </Page>
   );
 }
