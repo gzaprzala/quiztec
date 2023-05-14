832241b74522ec90c23dd51e46312fb4757d7982
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-05-14 22:38:25 +0200

    Added welcome message to homepage

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,24 +2,31 @@ import Page from '#components/Page/Page';
 import Button from '#components/Button/Button';
 import style from './Home.module.scss';
 import { Link } from 'react-router-dom';
+import { useSession } from '#providers/SessionProvider';
 
 export default function Home() {
+  const [session] = useSession();
+
   return (
     <Page>
       <div className={style.homeContainer}>
         <div className={style.homeText}>
-          <span className={style.homeTextUpper}>
-            Are you a true gamer? Take our quizzes and find out!
-          </span>
+          <span className={style.homeTextUpper}>Are you a true gamer? Take our quizzes and find out!</span>
           <span>Join the gaming community and show off your skills</span>
         </div>
         <div className={style.homeButtons}>
-          <Link to='/login'>
-            <Button className={style.homeButton}>SIGN IN</Button>
-          </Link>
-          <Link to='register'>
-            <Button className={style.homeButton}>SIGN UP</Button>
-          </Link>
+          {session.loggedIn ? (
+            <span>Welcome back {session.user?.username ?? '[USERNAME]'}!</span>
+          ) : (
+            <>
+              <Link to="/login">
+                <Button className={style.homeButton}>SIGN IN</Button>
+              </Link>
+              <Link to="register">
+                <Button className={style.homeButton}>SIGN UP</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </Page>
