2684c8e6a37e92c923455ce4590b29f20517ac3a
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-05-14 20:53:37 +0200

    Edited navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,10 +2,11 @@ import { Link } from 'react-router-dom';
 import style from './Navbar.module.scss';
 import MaterialSymbol from '#components/MaterialSymbol/MaterialSymbol';
 import { useState } from 'react';
+import { useSession } from '#providers/SessionProvider';
 
 const Navbar = () => {
+  const [session, { logout }] = useSession();
   const [menuActive, setMenuActive] = useState(false);
-  console.log(menuActive);
 
   return (
     <div className={style.navbarContainer}>
@@ -21,7 +22,17 @@ const Navbar = () => {
         </Link>
       </div>
       <div className={style.navbarRight}>
-        <MaterialSymbol symbol='account_circle' class={style.navbarIcon} />
+        {session.loggedIn && session.user?.profileImageUrl === null && (
+          <MaterialSymbol symbol='account_circle' class={style.navbarIcon} />
+        )}
+        {session.loggedIn && session.user?.profileImageUrl !== null && (
+          <img
+            className={style.navbarAvatar}
+            src={session.user?.profileImageUrl}
+            alt='?'
+          />
+        )}
+
         <MaterialSymbol
           symbol='menu'
           class={style.navbarMenuIcon}
@@ -32,16 +43,13 @@ const Navbar = () => {
         <div
           className={[
             style.navbarMenu,
-            menuActive ? style.navbarMenuDown : style.navbarMenuUp
-          ].join(' ')}
-        >
-          <Link to='/'>
-            Homepage
-          </Link>
+            menuActive ? style.navbarMenuDown : style.navbarMenuUp,
+          ].join(' ')}>
+          <Link to='/'>Homepage</Link>
 
-          <Link to='/categories'>
-            Categories
-          </Link>
+          <Link to='/categories'>Categories</Link>
+
+          {session.loggedIn && <span onClick={logout}>Logout</span>}
         </div>
       </div>
     </div>
