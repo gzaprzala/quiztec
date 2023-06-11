b823b34073b3792bff05220fea1c672bfcf4777a
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-06-12 00:43:20 +0200

    Updated http server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,22 @@
-import registerAliases from "module-alias";
+import registerAliases from 'module-alias';
 import 'dotenv/config';
 
-if (process.env.NODE_DEV !== "true") {
+if (process.env.NODE_DEV !== 'true') {
   registerAliases();
 }
 
-import { createServer, ViteDevServer } from "vite";
-import express from "express";
-import { accessControl, invalidRoute, logger } from "./server/middlewares";
-import { apiRouter } from "./server/routers/apiRouter";
-import compression, { CompressionOptions } from "compression";
-import {
-  frontendPath,
-  frontendProductionPath,
-  isDevMode,
-  serverPort,
-} from "#shared/constants";
-import chalk from "chalk";
-import path from "path";
-import { Database } from "#database/Database";
+import { createServer, ViteDevServer } from 'vite';
+import express from 'express';
+import { accessControl, generateSessionMiddleware, invalidRoute, logger } from './server/middlewares';
+import { apiRouter } from './server/routers/apiRouter';
+import compression, { CompressionOptions } from 'compression';
+import { frontendPath, frontendProductionPath, isDevMode, serverPort } from '#shared/constants';
+import chalk from 'chalk';
+import path from 'path';
 import { User as UserEntity } from '#database/entities/User';
-import session from "express-session";
-import RedisStore from 'connect-redis';
-import passport from "passport";
-
+import http from 'http';
+import passport from 'passport';
+import { SocketServer } from './server/SocketServer';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -47,6 +40,8 @@ const compressionOptions: CompressionOptions = {
 
 const main = async (): Promise<void> => {
   const app = express();
+  const server = http.createServer(app);
+
   let vite: ViteDevServer;
 
   app.use(accessControl);
@@ -54,52 +49,34 @@ const main = async (): Promise<void> => {
   app.use(logger);
   app.use(compression(compressionOptions));
 
-  app.use(session({
-    // secret: randomBytes(32).toString('hex'),
-    secret: 'SUPER_SECRET_SECRET',
-    resave: false,
-    saveUninitialized: false,
-    name: 'quiztec-auth',
-    rolling: true,
-    cookie: {
-      signed: true,
-      httpOnly: true,
-      maxAge: 30 * 60 * 1000,
-    },
-    store: new RedisStore({
-      client: await Database.getRedisClient(),
-      prefix: 'session_store:',
-    }),
-  }));
+  app.use(await generateSessionMiddleware());
 
   app.use(passport.initialize());
   app.use(passport.session());
 
-  app.use("/api", apiRouter);
+  app.use('/api', apiRouter);
 
   if (isDevMode) {
     vite = await createServer({
       server: { middlewareMode: true },
-      appType: "spa",
+      appType: 'spa',
     });
 
     app.use(vite.middlewares);
   } else {
     app.use(express.static(frontendProductionPath));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(frontendPath, "index.html"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(frontendPath, 'index.html'));
     });
   }
 
   app.use(invalidRoute);
 
-  app.listen(serverPort, () => {
-    console.log(
-      `${
-        isDevMode ? "Development server" : "Server"
-      } started on port ${chalk.green.bold(serverPort)}`
-    );
+  await SocketServer.createInstance(server);
+
+  server.listen(serverPort, () => {
+    console.log(`${isDevMode ? 'Development server' : 'Server'} started on port ${chalk.green.bold(serverPort)}`);
   });
 };
 
